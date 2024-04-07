6c47dc71bde2fb6503a569954633215bcd2f6516
Tjandra <tjandra.darmo@sawitpro.com>
2024-04-07 23:09:12 +0700

    feat[#5]: Implement mutex to prevent transaction race con

--- service/transaction/service.go
+++ service/transaction/service.go
@@ -9,6 +9,7 @@ import (
 	"money_transfer/repo"
 	"net/http"
 	"strconv"
+	"sync"
 )
 
 type TransactionServiceInterface interface {
@@ -17,18 +18,31 @@ type TransactionServiceInterface interface {
 }
 
 type TransactionService struct {
-	Repository repo.RepositoryInterface
-	Config     app.Config
+	Repository             repo.RepositoryInterface
+	Config                 app.Config
+	TransactionLocks       map[int]*sync.Mutex
+	TransactionStatusLocks map[string]*sync.Mutex
 }
 
 func NewTransactionService(repo repo.RepositoryInterface, config app.Config) TransactionService {
 	return TransactionService{
-		Repository: repo,
-		Config:     config,
+		Repository:             repo,
+		Config:                 config,
+		TransactionLocks:       map[int]*sync.Mutex{},
+		TransactionStatusLocks: map[string]*sync.Mutex{},
 	}
 }
 
 func (s TransactionService) CreateTransaction(input CreateTransactionInput) (bool, error) {
+	if mut, ok := s.TransactionLocks[int(input.AccountNumber)]; ok {
+		mut.Lock()
+		defer mut.Unlock()
+	} else {
+		s.TransactionLocks[int(input.AccountNumber)] = &sync.Mutex{}
+		s.TransactionLocks[int(input.AccountNumber)].Lock()
+		defer s.TransactionLocks[int(input.AccountNumber)].Unlock()
+	}
+
 	isSuccess := false
 	transactionInput := repo.Transaction{
 		RecipientAccountNumber: input.AccountNumber,
@@ -69,6 +83,15 @@ func (s TransactionService) CreateTransaction(input CreateTransactionInput) (boo
 }
 
 func (s TransactionService) ConfirmTransaction(transactionId string, isSuccess bool) error {
+	if mut, ok := s.TransactionStatusLocks[transactionId]; ok {
+		mut.Lock()
+		defer mut.Unlock()
+	} else {
+		s.TransactionStatusLocks[transactionId] = &sync.Mutex{}
+		s.TransactionStatusLocks[transactionId].Lock()
+		defer s.TransactionStatusLocks[transactionId].Unlock()
+	}
+
 	transactionIdInt, err := strconv.Atoi(transactionId)
 	if err != nil {
 		return errors.Join(constant.ErrInvalidTransactionId)
